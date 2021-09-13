b4efa061331e06a4c223c971f80ec1095cd15dec
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-09-13 20:16:40 +0300

    Spring security

---
+++ SpringSecurity/src/main/java/com/spring_security/SpringSecurity/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.spring_security.SpringSecurity;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+// This class has to extend a particular class to override an important method called "configure"
+
+@EnableWebSecurity // This tells spring security that this is a web secuirty configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // We need to set our configuration  to our auth objeect passed
+
+        // What kind of configuration we can do her
+
+        auth.inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER");
+
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
