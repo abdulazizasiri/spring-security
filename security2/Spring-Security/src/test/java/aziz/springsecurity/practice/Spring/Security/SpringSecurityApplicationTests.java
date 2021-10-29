b82b8ea0c7b593001c6fc1df4eeb696428fc75d3
Abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-10-30 00:41:24 +0300

    spring security project

---
+++ security2/Spring-Security/src/test/java/aziz/springsecurity/practice/Spring/Security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package aziz.springsecurity.practice.Spring.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
