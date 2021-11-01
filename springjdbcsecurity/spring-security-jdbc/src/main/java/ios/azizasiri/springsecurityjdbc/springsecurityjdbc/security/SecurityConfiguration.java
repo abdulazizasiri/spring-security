783b259d271cb46a32da6d3eac592231d3bd094a
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-11-01 09:08:25 +0300

    created an h2 database with users and passwords and then added some authorities to them

--- springjdbcsecurity/spring-security-jdbc/src/main/java/ios/azizasiri/springsecurityjdbc/springsecurityjdbc/security/SecurityConfiguration.java
+++ springjdbcsecurity/spring-security-jdbc/src/main/java/ios/azizasiri/springsecurityjdbc/springsecurityjdbc/security/SecurityConfiguration.java
@@ -1,8 +1,42 @@
 package ios.azizasiri.springsecurityjdbc.springsecurityjdbc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     // We have here two method to implment to configure authentication and authorities
-    
+    @Autowired
+    DataSource dataSource; // this is H2 dataSource
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth); // This deals with the spring security
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin(); //
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        System.out.println("PAssword Encoder was created");
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
