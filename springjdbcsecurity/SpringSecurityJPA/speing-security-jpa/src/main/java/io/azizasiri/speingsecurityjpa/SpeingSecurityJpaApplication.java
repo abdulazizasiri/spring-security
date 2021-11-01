2126d4da84fd9c1ae38ac801e965b1f43f0a850d
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-11-01 09:29:35 +0300

    spring security with jpa

---
+++ springjdbcsecurity/SpringSecurityJPA/speing-security-jpa/src/main/java/io/azizasiri/speingsecurityjpa/SpeingSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package io.azizasiri.speingsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeingSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeingSecurityJpaApplication.class, args);
+	}
+
+}
---
+++ springjdbcsecurity/SpringSecurityJPA/speing-security-jpa/src/test/java/io/azizasiri/speingsecurityjpa/SpeingSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.azizasiri.speingsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeingSecurityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
