e17880808737fc5e04b422c6cce25230baab1a14
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-11-01 08:28:15 +0300

    added security and one api resource

---
+++ springjdbcsecurity/spring-security-jdbc/src/main/java/ios/azizasiri/springsecurityjdbc/springsecurityjdbc/security/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package ios.azizasiri.springsecurityjdbc.springsecurityjdbc.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // We have here two method to implment to configure authentication and authorities
+    
+}
