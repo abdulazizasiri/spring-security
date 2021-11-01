e4def8ad56fd5c01604d69fa636cfafc629ac971
abdulazizasiri <asiriaabdulaziz@gmail.com>
2021-11-01 08:20:07 +0300

    spring security jdbc

---
+++ springjdbcsecurity/spring-security-jdbc/src/test/java/ios/azizasiri/springsecurityjdbc/springsecurityjdbc/SpringSecurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package ios.azizasiri.springsecurityjdbc.springsecurityjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
