eb794999359070d860fb81461582a0130824f491
abdulazizasiri <asiriaabdulaziz@gmail.com>
2022-01-26 11:22:19 +0300

    spring jpa security

--- newSpringSecurity/spring-security-jpa/spring-security-jpa/src/main/java/io/azizasiri/mysql/auth/springsecurityjpa/SpringSecurityJpaApplication.java
+++ newSpringSecurity/spring-security-jpa/spring-security-jpa/src/main/java/io/azizasiri/mysql/auth/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -1,9 +1,12 @@
 package io.azizasiri.mysql.auth.springsecurityjpa;
 
+import io.azizasiri.mysql.auth.springsecurityjpa.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SpringSecurityJpaApplication {
 
 	public static void main(String[] args) {
