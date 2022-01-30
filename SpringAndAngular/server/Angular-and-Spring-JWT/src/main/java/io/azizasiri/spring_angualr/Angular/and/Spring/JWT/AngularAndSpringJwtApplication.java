afec4a5a751b5f0beccfced6d4aa412a509cd66f
Abdulazizasiri <asiriaabdulaziz@gmail.com>
2022-01-31 01:54:08 +0300

    jwt in angular and spring

---
+++ SpringAndAngular/server/Angular-and-Spring-JWT/src/main/java/io/azizasiri/spring_angualr/Angular/and/Spring/JWT/AngularAndSpringJwtApplication.java
@@ -0,0 +1,13 @@
+package io.azizasiri.spring_angualr.Angular.and.Spring.JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularAndSpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularAndSpringJwtApplication.class, args);
+	}
+
+}
---
+++ SpringAndAngular/server/Angular-and-Spring-JWT/src/test/java/io/azizasiri/spring_angualr/Angular/and/Spring/JWT/AngularAndSpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.azizasiri.spring_angualr.Angular.and.Spring.JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularAndSpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
